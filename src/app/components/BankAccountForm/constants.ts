8bc7ef5570eb2774120d7dd45a5f8b0b3d38a1b5
Zoltan Maroti <zoli.maroti@gmail.com>
2025-04-11 12:55:08 +0200

    Currency and bank account selectors

---
+++ src/app/components/BankAccountForm/constants.ts
@@ -0,0 +1,8 @@
+import { AccountType } from "@/app/components/BankAccountCard/types";
+import { Currency } from "@/app/components/BankAccountCard/types";
+
+export const DEFAULT_BANK_ACCOUNT_TYPE: AccountType = "savings";
+
+export const DEFAULT_CURRENCY: Currency = "EUR";
+
+export const CURRENCIES: Currency[] = ["EUR", "USD", "GBP"];
