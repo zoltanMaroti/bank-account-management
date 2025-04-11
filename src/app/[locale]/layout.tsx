fceb9e38e04c8dcd52ccaded9f867274f764fb6c
Zoltan Maroti <zoli.maroti@gmail.com>
2025-04-11 09:59:46 +0200

    Configure i18n

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,24 @@
+import { NextIntlClientProvider, hasLocale } from "next-intl";
+import { notFound } from "next/navigation";
+import { routing } from "@/i18n/routing";
+
+export default async function LocaleLayout({
+    children,
+    params,
+}: {
+    children: React.ReactNode;
+    params: Promise<{ locale: string }>;
+}) {
+    const { locale } = await params;
+    if (!hasLocale(routing.locales, locale)) {
+        notFound();
+    }
+
+    return (
+        <html lang={locale}>
+            <body>
+                <NextIntlClientProvider>{children}</NextIntlClientProvider>
+            </body>
+        </html>
+    );
+}
