8a7d595622b2d0482ab5ae4e1e47c87df6b7714c
Zoltan Maroti <zoli.maroti@gmail.com>
2025-04-12 11:26:23 +0200

    Added search component

--- src/app/components/BankAccounts/BankAccounts.tsx
+++ src/app/components/BankAccounts/BankAccounts.tsx
@@ -5,14 +5,22 @@ import BankAccountCard from "@/app/components/BankAccountCard/BankAccountCard";
 import { BankAccount } from "@/app/components/BankAccountCard/types";
 import HorizontalScroll from "@/app/components/HorizontalScroll/HorizontalScroll";
 import CreateBankAccountButton from "@/app/components/CreateBankAccountButton/CreateBankAccountButton";
+import SearchBankAccount from "@/app/components/SearchBankAccount/SearchBankAccount";
+import { useTranslations } from "next-intl";
 
 const BankAccounts = ({
     initialAccounts,
 }: {
     initialAccounts: BankAccount[];
 }) => {
+    const t = useTranslations("DashboardPage");
+
     return (
-        <section className='flex flex-col gap-4 m-2 min-h-64'>
+        <section className='flex flex-col gap-2 m-2 min-h-64'>
+            <div className='flex justify-between flex-col gap-2 md:flex-row md:items-center'>
+                <h1 className='flex-1 text-2xl font-bold'>{t("title")}</h1>
+                <SearchBankAccount onChange={() => {}} searchTerm={""} />
+            </div>
             <div className='flex gap-2 w-full flex-col md:flex-row'>
                 <div className='flex-shrink-0'>
                     <CreateBankAccountButton />
